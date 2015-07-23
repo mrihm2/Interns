c01f864aa16328d6bbee9ee1ea31b2895b47ba1f
mrihm2 <mrihm@coredial.com>
2015-07-23 13:41:52 -0400

    Modified bankaccount file

--- BankAccount.java
+++ BankAccount.java
@@ -1,5 +1,21 @@
 package Testpkg;
 
+
 public class BankAccount {
+
+  private double balance;
+
+  public BankAccount(double balance) {
+    this.balance = balance;
+  }
+
+  public double debit(double amount) {
+    if (balance < amount) {
+      amount = balance;
+    }
+
+    balance -= amount;
+    return amount;
+  }
 
 }
